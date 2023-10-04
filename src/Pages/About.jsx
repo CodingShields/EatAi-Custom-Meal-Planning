b8e821bd16858c5aa570e441abdf53266e25a3b4
CodingShields <139907470+CodingShields@users.noreply.github.com>
2023-10-04 16:47:23 -0600

    A little about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function About() {
     return (
         <div className="about-container">
-            <h1> About Page Goes Here!</h1>
+            <h1> Welcome to ChefAi! Let the Chef help you with what most people struggle with, what to eat! Feel free to click around on the tabs to learn more!</h1>
            
         </div>
     )
