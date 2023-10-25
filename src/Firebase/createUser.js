24ae66db2a86525acc69ce49768c11f6bae405d6
MIke Shields <codingshields@gmail.com>
2023-10-24 19:14:21 -0600

    wefw

--- src/Firebase/createUser.js
+++ src/Firebase/createUser.js
@@ -2,7 +2,7 @@ import { doc, setDoc } from "firebase/firestore";
 import { UserAuth } from "../Context/AuthContext"
 
 const { user } = UserAuth()
-    
+    console.log("test",user);
 await setDoc(doc(db, "c", "LA"), {
   name: "Los Angeles",
   state: "CA",
